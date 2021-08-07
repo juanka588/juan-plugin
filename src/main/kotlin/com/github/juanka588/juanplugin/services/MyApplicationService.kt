b8019cce5ce6c8bcea5db2ad4d09f084d9ec4efe
GitHub Action <action@github.com>
2021-08-07 15:07:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/juanka588/juanplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/juanka588/juanplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.juanka588.juanplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.juanka588.juanplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/juanka588/juanplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/juanka588/juanplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.juanka588.juanplugin.services
 
+import com.github.juanka588.juanplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
